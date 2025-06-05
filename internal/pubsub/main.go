ce9df28169883c280c43029655d79fb575411ce1
nordluma <maxnordlund.95@hotmail.com>
2025-06-06 00:32:31 +0300

    feat(internal/pubsub): add publish json message

---
+++ internal/pubsub/main.go
@@ -0,0 +1,32 @@
+package pubsub
+
+import (
+	"context"
+	"encoding/json"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+	data, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	err = ch.PublishWithContext(
+		context.Background(),
+		exchange,
+		key,
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        data,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
