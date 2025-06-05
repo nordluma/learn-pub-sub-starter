46cf8e6c5818680dcfe33e29849a9262bb7fded6
nordluma <maxnordlund.95@hotmail.com>
2025-06-05 22:34:23 +0300

    feat(cmd/server): connect to rabbitmq and wait for `ctrl-c` to exit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	log.Println("Starting Peril server...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	log.Println("Connected to RabbitMQ")
+	<-done
+	log.Println("Shutting down Peril server")
 }
