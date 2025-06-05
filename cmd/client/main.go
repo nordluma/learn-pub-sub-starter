150a80288120eef4df54684bda70ce0b206ae464
nordluma <maxnordlund.95@hotmail.com>
2025-06-06 01:03:40 +0300

    feat(cmd/client): connect client to RabbitMQ

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	log.Println("Connected to RabbitMQ")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
