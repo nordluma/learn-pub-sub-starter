ccb0bdb15f3cecbbe74c95d527e02e81fdacca82
nordluma <maxnordlund.95@hotmail.com>
2025-06-09 18:15:19 +0300

    feat(cmd/client): declare and bind queue

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,12 +3,20 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 func main() {
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
 	fmt.Println("Starting Peril client...")
 	connStr := "amqp://guest:guest@localhost:5672/"
 	conn, err := amqp.Dial(connStr)
@@ -22,4 +30,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	_, _, err = pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, name),
+		routing.PauseKey,
+		pubsub.TransientQueue,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+	log.Println("Shutting down Peril client")
 }
