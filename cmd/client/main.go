e93e2d6d667e165ff4ca891bae2a663628968d25
nordluma <maxnordlund.95@hotmail.com>
2025-06-10 22:35:45 +0300

    feat(cmd/client): return `AckType` from message handlers

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,17 +10,35 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
-	return func(ps routing.PlayingState) {
+func handlerPause(
+	gs *gamelogic.GameState,
+) func(routing.PlayingState) pubsub.AckType {
+	return func(ps routing.PlayingState) pubsub.AckType {
 		defer fmt.Print("> ")
 		gs.HandlePause(ps)
+
+		return pubsub.Ack
 	}
 }
 
-func handlerMove(gs *gamelogic.GameState) func(gamelogic.ArmyMove) {
-	return func(mv gamelogic.ArmyMove) {
+func handlerMove(
+	gs *gamelogic.GameState,
+) func(gamelogic.ArmyMove) pubsub.AckType {
+	return func(mv gamelogic.ArmyMove) pubsub.AckType {
 		defer fmt.Print("> ")
-		gs.HandleMove(mv)
+
+		moveOutcome := gs.HandleMove(mv)
+		switch moveOutcome {
+		case gamelogic.MoveOutComeSafe:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeMakeWar:
+			return pubsub.Ack
+		case gamelogic.MoveOutcomeSamePlayer:
+			return pubsub.NackDiscard
+		default:
+			log.Printf("unknown move '%v'", moveOutcome)
+			return pubsub.NackDiscard
+		}
 	}
 }
 
